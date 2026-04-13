287347a62359c7576aa64f6a1df5959da3a8bdba
NithishKR4k <nr7505@srmist.edu.in>
2026-04-13 13:56:24 +0530

    helloApp

--- src/main/java/com/hello/HelloApp.java
+++ src/main/java/com/hello/HelloApp.java
@@ -2,6 +2,7 @@ package com.hello;
 
 public class HelloApp {
 	public static void main (String[] args) {
-		System.out.println("Hello World");
+		String name = args[0];
+		System.out.println("Hello " + name);
 	}
 }
